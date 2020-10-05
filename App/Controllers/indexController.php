fc1b9bb24e07c57accd3ccdad85cc0ed140db713
Rodrigo D <rodrigodalfre@gmail.com>
2020-10-05 16:37:15 -0300

    adicionando os layout

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -2,38 +2,24 @@
 
 namespace App\Controllers;
 
-class IndexController {
+use MF\Controller\Action;
 
-    private $view;
-
-    public function __construct(){
-        //A classe stdClass() é uma classe nativa do PHP.  
-        //Atraves dela é possível criar objetos padrões 
-        $this->view = new \stdClass();
-    }
+class IndexController extends Action {
 
     public function index(){
 
         $this->view->dados = array('Sofá', 'Cadeira', 'Cama');
         //o caminho é a partir de index.php
-        $this->render('index');
+        $this->render('index', 'layout1');
     }
 
     public function sobreNos(){
 
         $this->view->dados = array('Notebook',  'Celular');
-        $this->render('sobreNos');
+        $this->render('sobreNos', 'layout2');
     }
 
-    public function render($view){
- 
-        #echo get_class($this);
-        $classAtual = get_class($this);
-        $classAtual = str_replace('App\\Controllers\\', '', $classAtual);
-        $classAtual = strtolower(str_replace('Controller', '', $classAtual));
-        
-        require_once "../App/Views/".$classAtual."/".$view.".phtml";
-    }
+    
 }
 
 ?>
