654cc563b58453d995ca6fa2d306893ed4cc947e
Rodrigo D <rodrigodalfre@gmail.com>
2020-10-05 12:19:47 -0300

    criação dos views

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -4,12 +4,35 @@ namespace App\Controllers;
 
 class IndexController {
 
+    private $view;
+
+    public function __construct(){
+        //A classe stdClass() é uma classe nativa do PHP.  
+        //Atraves dela é possível criar objetos padrões 
+        $this->view = new \stdClass();
+    }
+
     public function index(){
-        echo 'Index Controller /Index';
+
+        $this->view->dados = array('Sofá', 'Cadeira', 'Cama');
+        //o caminho é a partir de index.php
+        $this->render('index');
     }
 
     public function sobreNos(){
-        echo 'Index Controller /sobreNos';
+
+        $this->view->dados = array('Notebook',  'Celular');
+        $this->render('sobreNos');
+    }
+
+    public function render($view){
+ 
+        #echo get_class($this);
+        $classAtual = get_class($this);
+        $classAtual = str_replace('App\\Controllers\\', '', $classAtual);
+        $classAtual = strtolower(str_replace('Controller', '', $classAtual));
+        
+        require_once "../App/Views/".$classAtual."/".$view.".phtml";
     }
 }
 
